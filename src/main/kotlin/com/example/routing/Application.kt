e1fda36a82b6d40ac72767b3822bbeefa17a4aac
Daniel <danielplimpias@gmail.com>
2023-07-09 21:58:58 -0400

    cambiamos la IP de 0.0.0.0 a 192.168.0.13

--- src/main/kotlin/com/example/routing/Application.kt
+++ src/main/kotlin/com/example/routing/Application.kt
@@ -6,7 +6,7 @@ import io.ktor.server.netty.*
 import com.example.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "192.168.0.13", module = Application::module)
         .start(wait = true)
 }
 
